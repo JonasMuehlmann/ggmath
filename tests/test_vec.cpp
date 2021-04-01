2bfc4499939bd9453e2c76c8db823cea0d6892f7
HollowProphet <49788305+HollowProphet@users.noreply.github.com>
2021-04-01 18:05:24 +0200

    test(vec): Add basic test for cross()

--- tests/test_vec.cpp
+++ tests/test_vec.cpp
@@ -4,4 +4,11 @@
 
 #include "vec.hpp"
 
-TEST(Vec, DefaultConstructVec2) {}
+TEST(Vec, VecCrossProduct)
+{
+    ggmath::vec::vec3f a             = ggmath::vec::vec3f(2, 3, 4);
+    ggmath::vec::vec3f b             = ggmath::vec::vec3f(5, 6, 7);
+    ggmath::vec::vec3f cross_product = ggmath::vec::cross(a, b);
+
+    ASSERT_EQ(cross_product, ggmath::vec::vec3f(-3, 6, -3));
+}
