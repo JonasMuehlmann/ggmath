f4a2584420e55bcea77b91cd8943dcd5d35853c6
HollowProphet <49788305+HollowProphet@users.noreply.github.com>
2021-04-04 14:33:11 +0200

    test(vec): add tests
    add tests for:
    - vec::angle_between()
    - vec::is_unit_vector()
    - vec::min()
    - vec::max()
    - vec::index_min()
    - vec::index_max()
    - vec::lerp()
    - vec::reflect()

--- tests/test_vec.cpp
+++ tests/test_vec.cpp
@@ -1,5 +1,7 @@
 #include <gtest/gtest.h>
 
+#include <numbers>
+
 #include "vec.hpp"
 using namespace ggmath;
 
@@ -25,6 +27,7 @@ TEST(Vec, LengthNegative)
 
     ASSERT_FLOAT_EQ(vec::length(zero_vector), 5.3851647);
 }
+
 
 TEST(Vec, LengthSquaredAny)
 {
@@ -67,6 +70,8 @@ TEST(Vec, NormalizedNegative)
 
     ASSERT_FLOAT_EQ(length(normalized), 1);
 }
+
+
 TEST(Vec, ScaledByAny)
 {
     vec::vec3f a = vec::vec3f(2, 3, 4);
@@ -88,6 +93,8 @@ TEST(Vec, ScaledByZeroVector)
 
     ASSERT_FLOAT_EQ(vec::length(b), 0);
 }
+
+
 TEST(Vec, ScaledToAny)
 {
     vec::vec3f a = vec::vec3f(2, 3, 4);
@@ -109,6 +116,7 @@ TEST(Vec, ScaledToZeroVector)
 
     ASSERT_FLOAT_EQ(vec::length(b), 0);
 }
+
 
 TEST(Vec, DistanceAny)
 {
@@ -137,10 +145,11 @@ TEST(Vec, DistanceFromOrigin)
     ASSERT_FLOAT_EQ(vec::distance(vec::vec3f(), a), vec::length(a));
 }
 
+
 TEST(Vec, ParallelAny)
 {
     vec::vec3f a           = vec::normalized(vec::vec3f(2, 3, 4));
-    vec::vec3f b           = vec::normalized(vec::vec3f(2, 3, 4));
+    vec::vec3f b           = vec::normalized(vec::vec3f(4, 6, 8));
     bool       is_parallel = vec::parallel(a, b);
 
     ASSERT_EQ(is_parallel, true);
@@ -148,7 +157,7 @@ TEST(Vec, ParallelAny)
 TEST(Vec, ParallelAnyNot)
 {
     vec::vec3f a           = vec::normalized(vec::vec3f(2, 3, 4));
-    vec::vec3f b           = vec::normalized(vec::vec3f(5, 6, 7));
+    vec::vec3f b           = vec::normalized(vec::vec3f(4, 6, 7));
     bool       is_parallel = vec::parallel(a, b);
 
     ASSERT_EQ(is_parallel, false);
@@ -162,10 +171,11 @@ TEST(Vec, ParallelZero)
     ASSERT_EQ(is_parallel, false);
 }
 
+
 TEST(Vec, AntiParallelAny)
 {
     vec::vec3f a           = vec::normalized(vec::vec3f(2, 3, 4));
-    vec::vec3f b           = vec::normalized(vec::vec3f(-2, -3, -4));
+    vec::vec3f b           = vec::normalized(vec::vec3f(-4, -6, -8));
     bool       is_parallel = vec::anti_parallel(a, b);
 
     ASSERT_EQ(is_parallel, true);
@@ -173,7 +183,7 @@ TEST(Vec, AntiParallelAny)
 TEST(Vec, AntiParallelAnyNot)
 {
     vec::vec3f a           = vec::normalized(vec::vec3f(2, 3, 4));
-    vec::vec3f b           = vec::normalized(vec::vec3f(5, 6, 7));
+    vec::vec3f b           = vec::normalized(vec::vec3f(4, 6, 7));
     bool       is_parallel = vec::anti_parallel(a, b);
 
     ASSERT_EQ(is_parallel, false);
@@ -186,6 +196,7 @@ TEST(Vec, AntiParallelZero)
 
     ASSERT_EQ(is_parallel, false);
 }
+
 
 TEST(Vec, PerpendicularAny)
 {
@@ -210,6 +221,166 @@ TEST(Vec, PerpendicularZero)
     bool       is_perpendicular = vec::perpendicular(a, zero_vector);
 
     ASSERT_EQ(is_perpendicular, true);
+}
+
+
+TEST(Vec, AngleBetweenAny)
+{
+    vec::vec3f a = vec::vec3f(2, 3, 4);
+    vec::vec3f b = vec::vec3f(4, 3, 2);
+
+    ASSERT_FLOAT_EQ(vec::angle_between(a, b), 0.531458);
+}
+TEST(Vec, AngleBetweenPerpendicular)
+{
+    vec::vec3f a = vec::normalized(vec::vec3f(1, -2, 3));
+    vec::vec3f b = vec::normalized(vec::vec3f(5, 4, 1));
+
+
+    ASSERT_FLOAT_EQ(vec::angle_between(a, b), 1.5707963);
+}
+TEST(Vec, AngleBetweenParallel)
+{
+    vec::vec3f a = vec::vec3f(2, 3, 4);
+    vec::vec3f b = vec::vec3f(4, 6, 8);
+
+    ASSERT_FLOAT_EQ(vec::angle_between(a, b), 0);
+}
+TEST(Vec, AngleBetweenAntiParallel)
+{
+    vec::vec3f a = vec::normalized(vec::vec3f(2, 3, 4));
+    vec::vec3f b = vec::normalized(vec::vec3f(-4, -6, -8));
+
+    ASSERT_FLOAT_EQ(vec::angle_between(a, b), std::numbers::pi);
+}
+
+
+TEST(Vec, IsUnitVectorAny)
+{
+    vec::vec3f a = vec::normalized(vec::vec3f(2, 3, 4));
+    ASSERT_EQ(vec::is_unit_vector(a), true);
+}
+TEST(Vec, IsUnitVectorAnyNot)
+{
+    vec::vec3f a = vec::vec3f(2, 3, 4);
+    ASSERT_EQ(vec::is_unit_vector(a), false);
+}
+TEST(Vec, IsUnitVectorZero)
+{
+    vec::vec3f a = vec::normalized(vec::vec3f());
+    ASSERT_EQ(vec::is_unit_vector(a), false);
+}
+
+
+TEST(Vec, MinAny)
+{
+    vec::vec3f a = vec::vec3f(4, 3, 2);
+
+    ASSERT_EQ(vec::min(a), 2);
+}
+
+
+TEST(Vec, MaxAny)
+{
+    vec::vec3f a = vec::vec3f(4, 3, 2);
+
+    ASSERT_EQ(vec::max(a), 4);
+}
+
+
+TEST(Vec, IndexMinAny)
+{
+    vec::vec3f a = vec::vec3f(4, 3, 2);
+
+    ASSERT_EQ(vec::index_min(a), 2);
+}
+
+
+TEST(Vec, IndexMaxAny)
+{
+    vec::vec3f a = vec::vec3f(4, 3, 2);
+
+    ASSERT_EQ(vec::index_max(a), 0);
+}
+
+
+TEST(Vec, LerpAny)
+{
+    vec::vec3f a = vec::vec3f(2, 3, 4);
+    vec::vec3f b = vec::vec3f(2, 6, 10);
+
+    vec::vec3f c = vec::lerp(a, b, 0.3);
+
+    ASSERT_EQ(c, vec::vec3f(2, 3.9, 5.8));
+}
+TEST(Vec, LerpTZero)
+{
+    vec::vec3f a = vec::vec3f(2, 3, 4);
+    vec::vec3f b = vec::vec3f(2, 6, 10);
+
+    vec::vec3f c = vec::lerp(a, b, 0);
+
+    ASSERT_EQ(c, a);
+}
+TEST(Vec, LerpTOne)
+{
+    vec::vec3f a = vec::vec3f(2, 3, 4);
+    vec::vec3f b = vec::vec3f(2, 6, 10);
+
+    vec::vec3f c = vec::lerp(a, b, 1);
+
+    ASSERT_EQ(c, b);
+}
+TEST(Vec, LerpTZeroFromZero)
+{
+    vec::vec3f a = vec::vec3f();
+    vec::vec3f b = vec::vec3f(2, 6, 10);
+
+    vec::vec3f c = vec::lerp(a, b, 0);
+
+    ASSERT_EQ(c, a);
+}
+TEST(Vec, LerpTOneFromZero)
+{
+    vec::vec3f a = vec::vec3f();
+    vec::vec3f b = vec::vec3f(2, 6, 10);
+
+    vec::vec3f c = vec::lerp(a, b, 1);
+
+    ASSERT_EQ(c, b);
+}
+
+TEST(Vec, ReflectAny)
+{
+    vec::vec3f a      = vec::normalized(vec::vec3f(1, 1, 0));
+    vec::vec3f normal = vec::vec3f(-1, 0, 0);
+    vec::vec3f b      = vec::reflect(a, normal);
+
+    ASSERT_EQ(b, vec::normalized(vec::vec3f(-1, 1, 0)));
+}
+TEST(Vec, ReflectPerpendicular)
+{
+    vec::vec3f a      = vec::vec3f(0, 0, 1);
+    vec::vec3f normal = vec::vec3f(-1, 0, 0);
+    vec::vec3f b      = vec::reflect(a, normal);
+
+    ASSERT_EQ(b, a);
+}
+TEST(Vec, ReflectAntiParallel)
+{
+    vec::vec3f a      = vec::vec3f(1, 0, 0);
+    vec::vec3f normal = vec::vec3f(-1, 0, 0);
+    vec::vec3f b      = vec::reflect(a, normal);
+
+    ASSERT_EQ(b, vec::vec3f(-1, 0, 0));
+}
+TEST(Vec, ReflectParallel)
+{
+    vec::vec3f a      = vec::normalized(vec::vec3f(2, 3, 4));
+    vec::vec3f normal = vec::vec3f(-1, 0, 0);
+    vec::vec3f b      = vec::reflect(a, normal);
+
+    ASSERT_EQ(b, b);
 }
 // endregion functions
 
